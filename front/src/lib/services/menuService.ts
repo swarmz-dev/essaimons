2c65ecb57221bdfaf1a9457b5a3df8ced233535c
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-09-17 20:27:02 +0200

    patched front deploy

--- front/src/lib/services/menuService.ts
+++ front/src/lib/services/menuService.ts
@@ -1,6 +1,6 @@
 import { m } from '#lib/paraglide/messages';
 import type { Component } from 'svelte';
-import { Home, Handshake, UserPen, LogOut, Lock, User, UsersRound, ChartColumn, ArrowLeftFromLine } from '@lucide/svelte';
+import { House, UserPen, LogOut, Lock, User, UsersRound, ChartColumn, ArrowLeftFromLine } from '@lucide/svelte';
 
 export type MenuItemsListItem = {
     href: string;
@@ -17,13 +17,8 @@ export const mainMenu: MenuItemsList = {
     connected: [
         {
             href: '/',
-            icon: Home,
+            icon: House,
             title: m['home.title'](),
-        },
-        {
-            href: '/social',
-            icon: Handshake,
-            title: m['social.title'](),
         },
         {
             href: '/profile',
