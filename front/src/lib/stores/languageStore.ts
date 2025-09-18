d2acd1f1a01b22a7a09f8462046c04b85f99eb94
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-09-18 12:50:37 +0200

    French default language

--- front/src/lib/stores/languageStore.ts
+++ front/src/lib/stores/languageStore.ts
@@ -2,4 +2,4 @@ import { type Writable, writable } from 'svelte/store';
 
 export type LanguageCode = 'en' | 'fr';
 
-export const language: Writable<LanguageCode> = writable('en');
+export const language: Writable<LanguageCode> = writable('fr');
